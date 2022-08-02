6bc569dabcab533a74ae001ee05a6ed99f77e4ed
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-08-02 14:07:22 +0900

    jsonデータに新規データを追加（未仕上）

--- app.rb
+++ app.rb
@@ -4,21 +4,67 @@ require 'json'
 
 # ------------------ read JSON --------------------
 json_file_path = 'data.json'
-json_data = open(json_file_path) do |io|
+$json_data = open(json_file_path) do |io|
   JSON.load(io)
 end
-memos = json_data['memos']
+$memos = $json_data['memos']
 # -------------------------------------------------
+def find_memo(dist_id)
+  dist_memo = ""
+  $memos.each do |memo|
+    dist_memo = memo if memo['id'] == dist_id
+  end 
+  return dist_memo
+end
 
-get '/' do
-  @memos = memos
+
+
+
+# top
+get '/memos' do
+  @memos = $memos
   erb :top
 end
 
-get '/top' do
-  erb :top
+
+# show
+get '/memos/:id/show' do
+  # make memos method!!!!!!!
+  
+  @memo = find_memo(params[:id])
+  erb :show
 end
 
-get '/hello/:name' do
-  "Hello0000 #{params['name']}!"
+
+# new
+get '/memos/new' do
+  erb :new
+end
+
+post '/memos/new' do
+  # calc next id
+  all_id = []
+  $memos.each do |memo|
+    all_id.push(memo['id'].to_i)
+  end
+  next_id = all_id.max + 1
+  
+  # make new json-data
+  new_data = {
+    "id" => next_id.to_s, 
+    "title" => params[:title], 
+    "content" => params[:content]
+  }
+
+
+  test1 = $json_data, Time.now()
+  @test1 = test1
+
+  # add data to json-file
+  $json_data['memos'].push(new_data)
+  
+  @test2 = $json_data
+
+
+  erb :test
 end
